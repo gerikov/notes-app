433d38b410f718896c9cf40187ecefc631adc766
gerikov <gerikov.mining@gmail.com>
2022-07-25 16:58:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const yargs = require("yargs");
+const chalk = require("chalk");
+//const getNotes = require("./notes.js");
+
+console.log(process.argv);
+console.log(yargs.argv);
+
+// const command = process.argv[2];
+
+// if (command === "add") {
+//   console.log("adding note");
+// } else if (command === "romve") {
+//   console.log("Remove note");
+
+// }
